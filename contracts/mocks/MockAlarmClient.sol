77cf11aa610afe5756a309216f8b2e2619eb3e56
Arthur <thearthurchen@gmail.com>
2021-03-24 00:07:27 -0700

    Mock alarm client

--- contracts/mocks/MockAlarmClient.sol
+++ contracts/mocks/MockAlarmClient.sol
@@ -1,8 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-contract MockAlarmClient {
-    constructor() public {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../IPact.sol";
 
+
+contract MockAlarmClient {
+
+    mapping( bytes32 => address ) requestIdToAddress;
+    uint count;
+
+    /**
+     * @dev
+     * Fall back check if no one checks the progress on the day that Pact ends
+     */
+    function cronJob(address pactAddress, uint64 endDateUtc) public  {
+        requestIdToAddress[bytes32(count)] = pactAddress;
+        count += 1;
     }
+
+    /**
+     * @dev Just convert address to bytes32 and it'll call the contract we want
+     */
+    function fulfill(bytes32 requestId) public {
+        address pactAddress = requestIdToAddress[requestId];
+        // Take the interface and call the contract's to check outcome
+        IPact pact = IPact(pactAddress);
+        pact.setFinished();
+    }
+
 }
