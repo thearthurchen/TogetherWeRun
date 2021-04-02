7ee23ba1ecb6c15af884f1b412926928768d6190
Tanner Velten <tannervelten@gmail.com>
2021-04-01 23:17:25 -0700

    Adds base EA to return dummy data and updates StravaClient interface

--- StravaExternalAdapter/app.js
+++ StravaExternalAdapter/app.js
@@ -1,44 +1,18 @@
-const {
-  createAthleteActivityRequest,
-  createRefreshTokenRequest
-} = require('./index');
+const createRequest = require('./index').createRequest
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.EA_PORT || 8080
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.post('/get_athlete_activity/:id', (req, res) => {
+app.post('/', (req, res) => {
   console.log('POST Data: ', req.body)
-  const { id } = req.params
-
-  res.status(200).json({
-    requestId: 123,
-    distance: 100,
-    id,
-    timestamp: Date.now()
-  });
-
-  // createAthleteActivityRequest({ id }, (status, result) => {
-  //   console.log('Result: ', result)
-
-  //   if (status === 400) {
-  //     createRefreshTokenRequest()
-  //   }
-
-  //   const { distance } = result.all_run_totals || {}
-  //   if (!distance) {
-  //     return res.status(404).json(result)
-  //   }
-
-  //   res.status(200).json({
-  //     distance,
-  //     id,
-  //     timestamp: Date.now() // TODO
-  //   })
-  // })
+  createRequest(req.body, (status, result) => {
+    console.log('Result: ', result)
+    res.status(status).json(result)
+  })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
--- StravaExternalAdapter/index.js
+++ StravaExternalAdapter/index.js
@@ -1,19 +1,4 @@
-/* eslint-disable standard/no-callback-literal */
-const { Requester, Validator } = require('@chainlink/external-adapter');
-// const dynamoose = require('dynamoose');
-
-// dynamoose.aws.sdk.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: process.env.REGION
-// });
-
-// const stravaUsers = dynamoose.model('StravaUsers', {
-//   userAddress: String,
-//   accessToken: String,
-//   refreshToken: String,
-//   userID: String
-// }, { create: false });
+const { Requester, Validator } = require('@chainlink/external-adapter')
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -22,91 +7,62 @@ const customError = (data) => {
   return false
 }
 
-const createAthleteActivityRequest = (input, cb) => {
-  const customParams = {
-    id: true
-  }
+// Define custom parameters to be used by the adapter.
+// Extra parameters can be stated in the extra object,
+// with a Boolean value indicating whether or not they
+// should be required.
+const customParams = {
+  user: ['user'],
+  timestamp: ['timestamp']
+}
 
-  const validator = new Validator(cb, input, customParams)
-  const { data: { id: userID, userAddress }, id: jobRunID } = validator.validated;
+// TODO (zack): query strava here
+const getStravaDistance = async (user, address) => {
+  // query strava
+  return 1.5
+}
 
-  const config = {
-    url: `https://www.strava.com/api/v3/athletes/${userID}/stats`,
-    headers: {
-      accept: 'application/json',
-      authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+const createRequest = async (input, callback) => {
+  // The Validator helps you validate the Chainlink request data
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  const {
+    user,
+    timestamp
+  } = validator.validated.data
+
+  const response = {
+    status: 200,
+    data: {
+      distance: await getStravaDistance(user, timestamp),
+      user,
+      timestamp
     }
   }
 
-  // DB connection (TODO)
-  stravaUsers.get(userAddress)
-    .then(res => console.log(res));
-
-  Requester.request(config, customError)
-    .then(response => {
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      cb(response.status, Requester.success(jobRunID, response))
-    })
-    .catch(error => {
-      cb(500, Requester.errored(jobRunID, error))
-    })
-}
-
-const createRefreshTokenRequest = (input, callback) => {
-  // Define custom parameters to be used by the adapter.
-  // Extra parameters can be stated in the extra object,
-  // with a Boolean value indicating whether or not they
-  // should be required.
-  const customParams = {
-    refresh_token: true
-  }
-
-  // The Validator helps you validate the Chainlink request data
-  const validator = new Validator(callback, input, customParams)
-
-  const { data: { refresh_token: refreshToken }, id: jobRunID } = validator.validated
-
-  const config = {
-    url: 'https://www.strava.com/oauth/token',
-    params: {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      refresh_token: refreshToken,
-      grant_type: 'refresh_token'
-    },
-    method: 'post'
-  }
-
-  // TODO: update user access token
-  Requester.request(config, customError)
-    .then(response => {
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
-    })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
+  response.data.result = Requester.validateResultNumber(response.data, ['distance'])
+  callback(response.status, Requester.success(jobRunID, response))
 }
 
 // This is a wrapper to allow the function to work with
 // GCP Functions
-const gcpservice = (req, res) => {
-  // createRequest(req.body, (statusCode, data) => {
-  //   res.status(statusCode).send(data)
-  // })
+exports.gcpservice = (req, res) => {
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
 }
 
 // This is a wrapper to allow the function to work with
 // AWS Lambda
-const handler = (event, context, callback) => {
-  // createRequest(event, (statusCode, data) => {
-  //   callback(null, data)
-  // })
+exports.handler = (event, context, callback) => {
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
 }
 
 // This is a wrapper to allow the function to work with
 // newer AWS Lambda implementations
-const handlerv2 = (event, context, callback) => {
+exports.handlerv2 = (event, context, callback) => {
   createRequest(JSON.parse(event.body), (statusCode, data) => {
     callback(null, {
       statusCode: statusCode,
@@ -118,10 +74,4 @@ const handlerv2 = (event, context, callback) => {
 
 // This allows the function to be exported for testing
 // or for running in express
-module.exports = {
-  createAthleteActivityRequest,
-  createRefreshTokenRequest,
-  gcpservice,
-  handler,
-  handlerv2
-}
+module.exports.createRequest = createRequest
