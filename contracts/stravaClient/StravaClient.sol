2c2deadf018c8c6ca2625ddf40f7f3a15dfb0bd0
Tanner Velten <tannervelten@gmail.com>
2021-04-02 18:27:27 -0700

    Compile sol code

--- contracts/stravaClient/StravaClient.sol
+++ contracts/stravaClient/StravaClient.sol
@@ -39,7 +39,7 @@ contract StravaClient is Ownable, ChainlinkClient {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestStravaData(address user, uint timestamp) internal returns (bytes32 requestId)
+    function requestStravaData(string memory user, string memory timestamp) internal returns (bytes32 requestId)
     {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
