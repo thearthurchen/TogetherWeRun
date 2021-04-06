5012211547569ac71de2fde26b53a23addf04bb1
Zack Woo <zwoo@fandango.com>
2021-04-05 21:32:46 -0700

    add external POST endpoint

--- StravaExternalAdapter/app.js
+++ StravaExternalAdapter/app.js
@@ -1,18 +1,40 @@
-const createRequest = require('./index').createRequest
+/* eslint-disable no-mixed-operators */
+const {
+  createAthleteActivityRequest,
+  createRefreshTokenRequest
+} = require('./index');
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.EA_PORT || 8080
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-  console.log('POST Data: ', req.body)
-  createRequest(req.body, (status, result) => {
-    console.log('Result: ', result)
-    res.status(status).json(result)
-  })
-})
+app.post('/get_athlete_activity/:id', (req, res) => {
+  const { id } = req.params
+
+  createRefreshTokenRequest({ data: { userAddress: id } }, (status, result) => {
+    const { jobRunID, data, result: { accessToken, userID }, statusCode } = result || {};
+
+    // console.log('refresh token request result', accessToken);
+
+    createAthleteActivityRequest({ data: { id: userID, accessToken } }, (status, result) => {
+      // console.log('Result: ', result.data.result.distance);
+
+      const { distance } = result.data.result || {};
+
+      if (isNaN(distance) && !distance) {
+        return res.status(404).json(result)
+      }
+
+      res.status(200).json({
+        distance,
+        id,
+        timestamp: Date.now() // TODO
+      });
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
