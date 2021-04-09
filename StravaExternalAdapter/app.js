6503ff5d85be861fedf3d63870f913cd9ac7bb0e
Zack Woo <zwoo@fandango.com>
2021-04-09 16:37:45 -0700

    allow CORS

--- StravaExternalAdapter/app.js
+++ StravaExternalAdapter/app.js
@@ -7,6 +7,12 @@ const port = process.env.EA_PORT || 8080
 
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/', (req, res) => {
   console.log('POST Data: ', req.body)
   createRequest(req.body, (status, result) => {
@@ -17,11 +23,14 @@ app.post('/', (req, res) => {
 
 app.post('/create-new-user', (req, res) => {
   const { code: accessCode } = req.body
+  console.log('CREATE NEW USER: ', accessCode)
   createNewUserRequest(accessCode, (result) => {
     if (result.status !== 200) {
-      return res.status(result.status).json('create user error');
+      console.log('CREATE NEW USER ERROR: ', result.status || 400)
+      return res.status(result.status || 400).json('create user error');
     }
 
+    console.log('CREATE NEW USER SUCCESS: ', result.status)
     return res.status(200).json('create user success');
   });
 });
